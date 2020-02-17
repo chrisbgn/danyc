aba448ca0fabf8bfd5ef7a86828600a01d2e986f
tianbo <christianbogarin@gmail.com>
2020-02-17 14:17:35 -0300

    agregado javascript

---
+++ eightball.js
@@ -0,0 +1,28 @@
+function GenerateAnswer() {
+  responses = [
+    "En mi opinión, sí",
+    "Es cierto",
+    "Es decididamente así",
+    "Probablemente",
+    "Buen pronóstico",
+    "Todo apunta a que sí",
+    "Sin duda",
+    "Sí",
+    "Sí - definitivamente",
+    "Debes confiar en ello",
+    "Respuesta vaga, vuelve a intentarlo",
+    "Pregunta en otro momento",
+    "Será mejor que no te lo diga ahora",
+    "No puedo predecirlo ahora",
+    "Concéntrate y vuelve a preguntar",
+    "Puede ser",
+    "No cuentes con ello",
+    "Mi respuesta es no",
+    "Mis fuentes me dicen que no",
+    "Las perspectivas no son buenas",
+    "Muy dudoso"
+  ];
+  response = responses[Math.floor(Math.random() * responses.length)];
+  document.getElementById('outputDiv').innerHTML = response;
+
+}
